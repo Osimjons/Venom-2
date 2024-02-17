36fe055fd476ef22293f3da3f33c83fac8d1cf06
Osimjon <osimjon90@ya.ru>
2024-02-17 20:31:19 +0300

    add modal trailer

--- script/main.js
+++ script/main.js
@@ -1,7 +1,22 @@
 const menuBurger = document.querySelector('.menu-burger');
 const menuList = document.querySelector('.header__list');
+const btnsSeeTrailer = document.querySelector('.btns__see-trailer');
+const modalTrailer = document.querySelector('.modal-trailer');
+const modalTrailerClose = document.querySelector('.modal-trailer__close');
+
 menuBurger.addEventListener('click', function () {
   menuBurger.classList.toggle('active');
   menuList.classList.toggle('active');
-
 });
+btnsSeeTrailer.addEventListener('click', function () {
+  modalTrailer.classList.add('activ');
+  modalTrailerClose.classList.remove('activ');
+  // menuList.classList.toggle('active');
+});
+modalTrailerClose.addEventListener('click', function () {
+  modalTrailer.classList.remove('activ');
+});
+modalTrailer.addEventListener('click', () => {
+  modalTrailer.classList.remove('activ');
+});
+
